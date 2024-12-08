2e62b8071caa2f2448c46c0ec154ae373943ba82
bssyousefi <b_s_s.yousefi@yahoo.com>
2024-12-07 22:47:10 -0500

    update leetcode/find-minimum-in-rotated-sorted-array

--- leetcode/find-minimum-in-rotated-sorted-array/solution.go
+++ leetcode/find-minimum-in-rotated-sorted-array/solution.go
@@ -1,7 +1,7 @@
 func findMin(nums []int) int {
     l := 0
     r := len(nums) - 1
-    
+
     for l < r {
         m := (l+r) / 2
         if nums[l] < nums[r] {
@@ -14,4 +14,20 @@ func findMin(nums []int) int {
         }
     }
     return nums[r]
+}
+// beats 100%
+func findMin(nums []int) int {
+    i, j := 0, len(nums) - 1
+    if nums[i] < nums[j] {
+        return nums[i]
+    }
+    for i < j {
+        m := (i + j) / 2
+        if nums[m] < nums[j] {
+            j = m
+        } else {
+            i = m + 1
+        }
+    }
+    return nums[i]
 }
--- leetcode/find-minimum-in-rotated-sorted-array/solution.py
+++ leetcode/find-minimum-in-rotated-sorted-array/solution.py
@@ -1,3 +1,4 @@
+# beats (100%)
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l = 0
@@ -11,4 +12,16 @@ class Solution:
             else:
                 r = m
         return nums[r]
-
+# beats (100%)
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if nums[0] < nums[-1]:
+            return nums[0]
+        i, j = 0, len(nums) - 1
+        while i < j:
+            m = (i+j) // 2
+            if nums[m] < nums[j]:
+                j = m
+            else:
+                i = m + 1
+        return nums[i]
