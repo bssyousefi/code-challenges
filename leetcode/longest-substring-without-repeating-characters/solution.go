6c959ae4c76e1c5066b7b0a2f005d698c823d7db
bssyousefi <b_s_s.yousefi@yahoo.com>
2024-12-10 00:15:52 -0500

    update leetcode/longest-substring-without-repeating-characters

--- leetcode/longest-substring-without-repeating-characters/solution.go
+++ leetcode/longest-substring-without-repeating-characters/solution.go
@@ -1,3 +1,4 @@
+// Previous solution (beats 100%)
 func lengthOfLongestSubstring(s string) int {
     cache := make(map[rune]int);
     m := 0
@@ -14,5 +15,25 @@ func lengthOfLongestSubstring(s string) int {
         return max(m, len(s) - j)
     } else {
         return len(s)
+    }
+}
+// solution (beats 81%)
+func lengthOfLongestSubstring(s string) int {
+    _max := 0
+    _min_index := 0
+    cache := map[byte]int{}
+    for i := range s {
+        if j, ok := cache[s[i]]; ok && j >= _min_index {
+            if i - _min_index > _max {
+                _max = i - _min_index
+            }
+            _min_index = j + 1
+        }
+        cache[s[i]] = i
+    }
+    if len(s) - _min_index > _max {
+        return len(s) - _min_index
+    } else {
+        return _max
     }
 }
--- leetcode/longest-substring-without-repeating-characters/solution.py
+++ leetcode/longest-substring-without-repeating-characters/solution.py
@@ -1,3 +1,4 @@
+# Previous solution (beats 89%)
 def lengthOfLongestSubstring(self, s: str) -> int:
         cache = {}
         j = 0
@@ -8,3 +9,22 @@ def lengthOfLongestSubstring(self, s: str) -> int:
                 j = cache[s[i]]+1
             cache[s[i]] = i
         return max(m, len(s) - j) if m > 0 else len(s)
+
+# Other solution (beats 62%)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        _max = 0
+        _map = defaultdict(int)
+        i, j = 0, 0
+        while j < len(s):
+            if _map[s[j]] != 0:
+                if j - i > _max:
+                    _max = j - i
+            while _map[s[j]] != 0:
+
+                _map[s[i]] -= 1
+                i += 1
+
+            _map[s[j]] += 1
+            j += 1
+        return _max if j - i < _max else j - i
