9e72f996637115caa4bcd6c3c60155c8e4fb3b35
bssyousefi <b_s_s.yousefi@yahoo.com>
2024-12-09 23:21:10 -0500

    update leetcode/best-time-to-buy-and-sell-stock

--- leetcode/best-time-to-buy-and-sell-stock/solution.go
+++ leetcode/best-time-to-buy-and-sell-stock/solution.go
@@ -1,7 +1,8 @@
+// Solution (beats 100%)
 func maxProfit(prices []int) int {
     var min_ int = 1e7
     max_ := 0
-    
+
     for _, price := range prices {
         if price < min_ {
             min_ = price
--- leetcode/best-time-to-buy-and-sell-stock/solution.py
+++ leetcode/best-time-to-buy-and-sell-stock/solution.py
@@ -1,3 +1,4 @@
+# Previous solution (beats 67%)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_ = 1e7
@@ -9,3 +10,27 @@ class Solution:
             max_ = max(max_, prices[i] - min_)
         return max_
 
+# Other solution (beats 38%)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        _max = 0
+        i,j = 0, 0
+        while j < len(prices):
+            if prices[j] < prices[i]:
+                i = j
+            else:
+                _max = max(_max, prices[j] - prices[i])
+            j += 1
+        return _max
+
+# Third solution (beats 92%)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_ = 1e7
+        max_ = 0
+        for price in prices:
+            if price < min_:
+                min_ = price
+            elif price - min_ > max_:
+                max_ = price - min_
+        return max_
