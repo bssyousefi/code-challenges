1d41bdbdccf2b6c02a893f0b28e7c4cd1a3962d2
bssyousefi <b_s_s.yousefi@yahoo.com>
2024-12-14 12:07:35 -0500

    update leetcode/sliding-window-maximum

--- leetcode/sliding-window-maximum/solution.go
+++ leetcode/sliding-window-maximum/solution.go
@@ -1,3 +1,4 @@
+// First solution (beats 94%) (better queue implementation)
 func maxSlidingWindow(nums []int, k int) []int {
     ret := make([]int, 0, len(nums)-k+1)
     maxs := make([]int, 0)
@@ -18,4 +19,33 @@ func maxSlidingWindow(nums []int, k int) []int {
         r++
     }
     return ret
+}
+// Second solution (beats 60%)
+func maxSlidingWindow(nums []int, k int) []int {
+    l,r := 0, 0
+    maxs := []int{}
+    count := 0
+    res := []int{}
+    for r < len(nums) {
+        for count > 0 && nums[r] > nums[maxs[count-1]] {
+            count--
+        }
+        if count >= len(maxs) {
+            maxs = append(maxs, r)
+        } else {
+            maxs[count] = r
+        }
+        count++
+
+        if l > maxs[0] {
+            maxs = maxs[1:]
+            count--
+        }
+        if r-l+1 == k {
+            l++
+            res = append(res, nums[maxs[0]])
+        }
+        r++
+    }
+    return res
 }
--- leetcode/sliding-window-maximum/solution.py
+++ leetcode/sliding-window-maximum/solution.py
@@ -1,3 +1,4 @@
+# First solution (beats 5%) (better than 2nd)
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
@@ -20,3 +21,48 @@ class Solution:
         
         return res
 
+# Second solution (beats 5%)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        _max = []
+        res = []
+        for r in range(k):
+            while len(_max) > 0 and nums[r] > _max[-1]:
+                _max.pop(-1)
+            _max.append(nums[r])
+        res = [_max[0]]
+        l = 0
+        while r < len(nums) - 1:
+            if nums[l] == _max[0]:
+                _max.pop(0)
+            l += 1
+            r += 1
+            while len(_max) > 0 and nums[r] > _max[-1]:
+                _max.pop(-1)
+            _max.append(nums[r])
+            res.append(_max[0])
+
+        return res
+
+# Third solution (beats 43%) (use deque)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        maxs = deque()
+        m = -1e7
+        l = 0
+        r = 0
+        while r < len(nums):
+            while maxs and nums[maxs[-1]] < nums[r]:
+                maxs.pop()
+            maxs.append(r)
+
+            if l > maxs[0]:
+                maxs.popleft()
+
+            if r>=k-1:
+                res.append(nums[maxs[0]])
+                l += 1
+            r += 1
+        
+        return res
