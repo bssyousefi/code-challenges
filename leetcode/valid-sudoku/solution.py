406fb90e10877bb248e67849d79f95f39aac7ee2
bssyousefi <b_s_s.yousefi@yahoo.com>
2025-10-25 20:09:13 -0400

    update leetcode/valid-sudoku

--- leetcode/valid-sudoku/solution.py
+++ leetcode/valid-sudoku/solution.py
@@ -21,3 +21,31 @@ class Solution:
                 else:
                     _map[f'{i//3}-{j//3}'].add(v)
         return True
+
+# Second solution (beats 81%)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        squares = [[set() for i in range(3)] for _ in range(3)]
+
+        for row in range(9):
+            for col in range(9):
+                val = board[row][col]
+                if val == ".":
+                    continue
+                if val in rows[row]:
+                    return False
+                else:
+                    rows[row].add(val)
+
+                if val in cols[col]:
+                    return False
+                else:
+                    cols[col].add(val)
+
+                if val in squares[row//3][col//3]:
+                    return False
+                else:
+                    squares[row//3][col//3].add(val)
+        return True
