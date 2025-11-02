aa81e7fe9d605a5ab0f14986b27f7909ed19a4bf
bssyousefi <b_s_s.yousefi@yahoo.com>
2025-11-02 17:20:15 -0500

    update leetcode/maximum-subarray

--- leetcode/maximum-subarray/solution.py
+++ leetcode/maximum-subarray/solution.py
@@ -13,3 +13,19 @@ class Solution:
                 max_ = cur
 
         return max_
+
+# Second solution (beats 98%) (same solution)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_ = nums[0]
+        max_t = nums[0]
+        for i in range(1, len(nums)):
+            v = nums[i]
+            if max_t >= 0:
+                max_t += v
+            else:
+                max_t = v
+            if max_t > max_:
+                max_ = max_t
+
+        return max_
