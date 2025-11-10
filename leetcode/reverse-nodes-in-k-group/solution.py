e35a2233f2749f1d8c577c1abc892003114d7bcb
bssyousefi <b_s_s.yousefi@yahoo.com>
2025-11-10 13:46:30 -0500

    update leetcode/reverse-nodes-in-k-group

--- leetcode/reverse-nodes-in-k-group/solution.py
+++ leetcode/reverse-nodes-in-k-group/solution.py
@@ -60,3 +60,27 @@ class Solution:
                 pre.next = head
 
         return main_head.next
+
+# Second solution (beats 100%)
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        root = ListNode()
+        cur = root
+        counter = 0
+        tmp = None
+        while head:
+            counter += 1
+            node = ListNode(head.val, tmp)
+            tmp = node
+            if counter == 1:
+                first = node
+                cur.next = head
+            if counter == k:
+                cur.next = tmp
+                cur = first
+                tmp = None
+                counter = 0
+            head = head.next
+
+        return root.next
+
