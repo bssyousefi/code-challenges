0ab4e5afe8c43035f8df4e8d7f5cd8eb90918508
bssyousefi <b_s_s.yousefi@yahoo.com>
2026-02-10 20:21:46 -0500

    update leetcode/number-of-islands

--- leetcode/number-of-islands/solution.py
+++ leetcode/number-of-islands/solution.py
@@ -95,3 +95,29 @@ class Solution:
                     dfs(i, j)
 
         return ret
+
+# Fifth solution (beats 89%) (DFS)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        visit = [[False] * n for _ in range(m)]
+        islands_count = 0
+        directions = [(1, 0), (0,1), (-1, 0), (0, -1)]
+
+        def find(i, j):
+            visit[i][j] = True
+            for di, dj in directions:
+                ii = di + i
+                jj = dj + j
+                if ii >= 0 and ii < m and jj >= 0 and jj < n and grid[ii][jj] == "1" and not visit[ii][jj]:
+                    find(ii, jj)
+
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and not visit[i][j]:
+                    islands_count += 1
+                    find(i, j)
+
+        return islands_count
