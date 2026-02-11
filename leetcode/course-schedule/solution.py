75e92ee66f681cccd65e0589081d50968a84aefa
bssyousefi <b_s_s.yousefi@yahoo.com>
2026-02-10 22:11:10 -0500

    update leetcode/course-schedule

--- leetcode/course-schedule/solution.py
+++ leetcode/course-schedule/solution.py
@@ -24,3 +24,32 @@ class Solution:
                     return False
 
         return True
+
+# Second solution (beats 87%) (DFS)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visit = [0] * numCourses
+        paths = [[] for _ in range(numCourses)]
+        for i, j in prerequisites:
+            paths[j].append(i)
+
+        def dfs(i):
+            for j in paths[i]:
+                if visit[j] == 0:
+                    visit[j] = 1
+                    if not dfs(j):
+                        return False
+                    visit[j] = 2
+                elif visit[j] == 1:
+                    return False
+            return True
+
+        for i in range(numCourses):
+            if visit[i] == 0:
+                visit[i] = 1
+                if not dfs(i):
+                    return False
+                visit[i] = 2
+
+        return True
+
